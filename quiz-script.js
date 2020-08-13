a12f600eabc32893d384d78bb6c247256d9a7a5b
Nathan Schmitzer <nathan.schmitzer@yahoo.com>
2020-08-13 17:23:57 -0500

    saving starting code before computer restart

---
+++ quiz-script.js
@@ -0,0 +1,13 @@
+var questions = {
+    question: "What does ", 
+    correct: "right answer", 
+    incorrect: "not right",
+    incorrect2: "wrong",
+    incorrect3: "also wrong" 
+}
+
+var timer = 0;
+
+var correctAnswer = 0;
+
+var questionsAnswered = 0;
